161397176c5838f3dabade78008c55472ea43494
@dewani12 <gauravdewani2@gmail.com>
2025-02-06 01:38:07 +0530

    fix: update code-change event to use broadcast for emitting changes to room

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ io.on("connection", (socket) => {
   });
   socket.on("code-change", ({ roomId, code, socketId }) => {
     console.log("code-change", code, "by socketId", socketId?socketId:"k");
-    socket.to(roomId).emit("recieve-changes", code);
+    socket.broadcast.to(roomId).emit("recieve-changes", code);
   });
   socket.on("structure-change", ({ roomId, structure }) => {
     console.log("structure-change", structure);
